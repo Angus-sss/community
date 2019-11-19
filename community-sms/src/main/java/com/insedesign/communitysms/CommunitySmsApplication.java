6512b7d1b78d405dc804285dd0456fd49d0e2b8e
nalhoug <914374385@qq.com>
2019-11-19 15:46:34 +0800

    添加消息模块

---
+++ community-sms/src/main/java/com/insedesign/communitysms/CommunitySmsApplication.java
@@ -0,0 +1,13 @@
+package com.insedesign.communitysms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunitySmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CommunitySmsApplication.class, args);
+    }
+
+}
---
+++ community-sms/src/test/java/com/insedesign/communitysms/CommunitySmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.insedesign.communitysms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunitySmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
